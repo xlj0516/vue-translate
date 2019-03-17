db5ffc515a2a9956c19b50ea9edf47e9c7a9da48
xlj0516 <1060541678.qq.com>
2019-03-17 23:04:22 +0800

    first commit

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="translateForm">
+   <form v-on:submit="formSubmit">
+        <input type="text" v-model="textToTranslate" placeholder="输入需要翻译的内容">
+        <select v-model="language">
+          <option value="en">English</option>
+          <option value="ru">Russian</option>
+          <option value="ko">Korean</option>
+          <option value="ja">Janpenese</option>
+        </select>
+        <input type="submit" value="翻译">
+   </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data:function(){
+      return{
+          textToTranslate:"",
+          language:""
+      }
+  },
+  methods:{
+      formSubmit:function(e){
+          //alert(this.textToTranslate);
+          this.$emit("formSubmit",this.textToTranslate,this.language);
+          e.preventDefault();
+      }
+  },
+  created:function(){
+      this.language="en";
+  }
+}
+</script>
+
+<style>
+
+</style>
